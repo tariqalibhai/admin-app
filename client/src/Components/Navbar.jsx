9a5d6c784404cf4e6bda8ec346211a4f5553a7dc
Student ApnaCollege <tariqalidon74@gmail.com>
2024-10-17 16:10:22 +0500

    add: adding 2nd push

--- client/src/Components/Navbar.jsx
+++ client/src/Components/Navbar.jsx
@@ -10,7 +10,7 @@ export const Navbar = () => {
       <header>
         <div className="container">
           <div className="logo-brand">
-            <NavLink to="/">ThapaTechnical</NavLink>
+            <NavLink to="/">SMART-CITY</NavLink>
           </div>
 
           <nav>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -9,10 +9,10 @@ export const Home = () => {
           <div className="container grid grid-two-cols">
             <div className="hero-content">
               <p>We are the World Best IT Company</p>
-              <h1>Welcome to Thapa Technical</h1>
+              <h1>Welcome to SMART-CITY</h1>
               <p>
                 Are you ready to take your business to the next level with
-                cutting-edge IT solutions? Look no further! At Thapa Technical,
+                cutting-edge IT solutions? Look no further! At SMART-CITY,
                 we specialize in providing innovative IT services and solutions
                 tailored to meet your unique needs.
               </p>
